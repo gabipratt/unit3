2adc2a9a45a5b887428f89c7f1d31366b1e616b3
gabipratt <Prattingerova.gabi@yahoo.com>
2018-02-20 18:38:36 +0100

    change

--- distance_measuring_sensor_test/distance_measuring_sensor_test.ino
+++ distance_measuring_sensor_test/distance_measuring_sensor_test.ino
@@ -31,10 +31,10 @@ void loop() {
   val = analogRead(sensorpin);       // reads the value of the sharp sensor
   Serial.println(val);            // prints the value of the sensor to the serial monitor
   delay(400);                    // wait for this much time before printing next value 
- if ( val > 300){
-  myDFPlayer.volume(5);
+ if ( val > 300){ // how close to the sensor it gets triggered
+   myDFPlayer.volume(10);
    myDFPlayer.play(1);
-   delay (5000);
+   delay (5000); //how long will the sound play
  }
   else 
   myDFPlayer.stop();
