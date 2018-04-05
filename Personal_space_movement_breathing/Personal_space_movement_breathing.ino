8b61884624b27c194c3a3b1bcd9be06783a1210a
ananditapunj <ananditapunj@gmail.com>
2018-04-05 17:43:19 +0200

    Breathing & motor

---
+++ Personal_space_movement_breathing/Personal_space_movement_breathing.ino
@@ -0,0 +1,101 @@
+#include <Servo.h>
+
+#include "PersonalSpace.cpp"
+#define READ_DELAY 100 //Delay between readings in milliseconds
+
+#define CHB_DIR 13
+#define CHB_PWM 11
+#define CHB_BRK 8
+#define CHB_SNS A1
+
+#define CHA_DIR 12
+#define CHA_PWM 3
+#define CHA_BRK 9
+#define CHA_SNS A0
+
+PersonalSpace personalSpace;
+Servo myservo;  // create servo object to control a servo
+// twelve servo objects can be created on most boards
+
+int pos = 0;    // variable to store the servo position
+
+
+void init_motor() {
+  // initialize the PWM pins
+  pinMode(CHB_PWM, OUTPUT);
+  digitalWrite(CHB_PWM, LOW);
+  // initialize the PWM pins
+  pinMode(CHA_PWM, OUTPUT);
+  digitalWrite(CHA_PWM, LOW);
+
+  pinMode(CHA_DIR, OUTPUT);
+  pinMode(CHA_BRK, OUTPUT);
+  digitalWrite(CHA_DIR, LOW);
+  digitalWrite(CHA_BRK, LOW);
+  pinMode(CHB_DIR, OUTPUT);
+  pinMode(CHB_BRK, OUTPUT);
+  digitalWrite(CHB_DIR, LOW);
+  digitalWrite(CHB_BRK, LOW);
+
+  Serial.println( "initializing motor" );       
+
+
+                                                                                                          
+}
+void setup()
+{
+  //Start the serial connection
+  Serial.begin(9600);
+   init_motor();
+    myservo.attach(5);  // attaches the servo on pin 5 to the servo object
+}
+
+void loop()
+{
+
+  personalSpace.measure_distance();
+
+  // this ensures that the robot only reacts within the sight range
+  if (personalSpace.isInRange()) {
+
+    if (personalSpace.isInDangerDistance()) {
+      // here you will trigger the shutdown system
+      Serial.println("DANGER!");
+      analogWrite(CHB_PWM, random(0,255));
+      analogWrite(CHA_PWM, random(0,255));
+
+      //servo movement
+for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+
+
+
+      
+    }
+    }    else {
+      Serial.print("Proximity percantage = ");
+      float perc = personalSpace.getProximityPercentage();
+      Serial.println(perc);
+      analogWrite(CHB_PWM, map(perc,0,100,0,255));
+      analogWrite(CHA_PWM, map(perc,0,100,0,255));
+      //This shows the range in percentage from 0 to 100% within the range untill the DANGER range
+    }
+
+
+  } else {
+    Serial.println("Can't see anyone");
+    //Range above the defined sight range
+     analogWrite(CHB_PWM, 0);
+      analogWrite(CHA_PWM, 0);
+  }
+
+  //Delay before the next reading
+  delay(READ_DELAY);
+}
+
